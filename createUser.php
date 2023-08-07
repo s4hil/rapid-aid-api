86e2d680fdfc4bcb6af5a5fbe734cbe5f3ede6fd
s4hil <s4hilp4rr4y@gmail.com>
2023-08-07 12:21:23 +0530

    init

---
+++ createUser.php
@@ -0,0 +1,37 @@
+<?php
+	include 'db.php';
+
+	$response = array();
+
+	$data = json_decode(file_get_contents("php://input"), true);
+
+	if (!empty($data)) {
+
+		$name = $data['name'];
+		$email = $data['email'];
+		$password = $data['password'];
+		$coordinates = $data['coordinates'];
+		$address = $data['address'];
+
+		$email = $data['email'];
+		$password = $data['password'];
+
+		$sql = "INSERT INTO '_users' (`name`,`email`,`password`,`coordinates`,`address`) VALUES(
+				'$name',
+				'$email',
+				'$password',
+				'$coordinates',
+				'$address'
+			)";
+		$res = $db->exec($sql);
+		if ($res) {
+				$response['status'] = true;
+				$response['data'] = "Account Created!";
+		}
+	}
+	else{
+		$response['status'] = false;
+		$response['data'] = "Input not given";
+	}				
+	echo json_encode($response);
+?>
